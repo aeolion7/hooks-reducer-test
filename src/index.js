383b99d21633e745013399434f5e7207277b02eb
aeolion7 <joseph.lionarons@cedrus.digital>
2019-06-10 11:44:36 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Count = () => {
+  /*
+    useReducer needs two arguments.
+    The first is a reducer function, the second is the initial state.
+    It returns an array of two elements.
+    The first is the current state, the second is a dispatch function.
+  */
+  const [count, dispatch] = useReducer((state, action) => {
+    switch (action) {
+      case 'INC':
+        return state + 1;
+      case 'DEC':
+        return state - 1;
+      default:
+        return state;
+    }
+  }, 0);
+  return (
+    <>
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          dispatch('INC');
+        }}
+      >
+        Increment
+      </button>
+      <button
+        onClick={() => {
+          dispatch('DEC');
+        }}
+      >
+        Decrement
+      </button>
+    </>
+  );
+};
+
+ReactDOM.render(<Count />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
